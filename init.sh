149fa7c5097e25390fb95aa4ba06a0c447d76aa4
acaranta <arthur@caranta.com>
2023-01-07 15:41:37 +0100

    forced disabling of hostkey checking

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ if [ "$REMOTE" != "true" ]; then
 	/usr/bin/autossh \
 		-vv \
 		-o StrictHostKeyChecking=no \
+		-o UserKnownHostsFile=/dev/null \
 		-Nn $TUNNEL_HOST \
 		-p $TUNNEL_PORT \
 		-L *:$LOCAL_PORT:$REMOTE_HOST:$REMOTE_PORT \
@@ -11,6 +12,7 @@ else
 	/usr/bin/autossh \
 		-vv \
 		-o StrictHostKeyChecking=no \
+		-o UserKnownHostsFile=/dev/null \
 		-Nn $TUNNEL_HOST \
 		-p $TUNNEL_PORT \
 		-R 0.0.0.0:$REMOTE_PORT:$CONTAINER_HOST:$CONTAINER_PORT \
